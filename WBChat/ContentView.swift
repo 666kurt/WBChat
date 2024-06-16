6e0fcdaace697158c4cd693a5473d7ea6f7aa2ce
Maxim <incentivedie@gmail.com>
2024-06-16 20:35:51 +0400

    clear main branch

---
+++ WBChat/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WBChat
+//
+//  Created by Максим Шишлов on 16.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, WBChat!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
