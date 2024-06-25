15bd3aedf1d2b3bd364251abfbfbb0b989ead357
Maxim <incentivedie@gmail.com>
2024-06-25 13:05:33 +0400

    Fixing issures

--- WBChat/WBChat_HW5.playground/Contents.swift
+++ WBChat/WBChat_HW5.playground/Contents.swift
@@ -1,26 +1,46 @@
 import Foundation
 
-// MARK: Паспорт
+// MARK: - Протоколы
 
 // Протокол для идентифицируемого объекта
 protocol Identifiable {
     var id: String { get }
 }
 
+// Протокол для паспорта
+protocol PassportProtocol: Identifiable {
+    var series: String { get }
+    var number: Int { get }
+    var issueDate: Date { get }
+}
+
+// Протокол для человека
+protocol PersonProtocol {
+    var fullName: String { get }
+    var age: Int { get }
+    var passport: PassportProtocol? { get set }
+}
+
+// MARK: - Классы
+
 // Класс, описывающий паспорт
-final class Passport: Identifiable {
+final class Passport: PassportProtocol {
     let series: String
     let number: Int
-    let date: Date
+    let issueDate: Date
     weak var person: Person? // weak для избежания сильного цикла ссылок
     var id: String {
         return "\(series) \(number)"
     }
     
-    init(series: String, number: Int, date: Date, person: Person? = nil) {
+    init(series: String, number: Int, issueDate: Date, person: Person? = nil) {
+        // Проверка корректности серии и номера
+        guard series.count == 2, number > 0 else {
+            fatalError("Некорректные серия или номер паспорта")
+        }
         self.series = series
         self.number = number
-        self.date = date
+        self.issueDate = issueDate
         self.person = person
         print("Инициализация паспорта \(id)")
     }
@@ -30,46 +50,36 @@ final class Passport: Identifiable {
     }
 }
 
-// MARK: Человек
-
-// Протокол для определения класса Person
-protocol PersonProtocol {
-    var fullName: String { get }
-    var age: Int { get }
-    var passport: Identifiable? { get set }
-}
-
 // Класс, описывающий человека
 final class Person: PersonProtocol {
     var fullName: String
     let age: Int
-    private(set) var passwordNumber: Int
-    var passport: Identifiable?
+    private(set) var passportNumber: Int
+    var passport: PassportProtocol?
     
-    init(fullName: String, age: Int, passwordNumber: Int, passport: Passport? = nil) {
+    init(fullName: String, age: Int, passportNumber: Int, passport: Passport? = nil) {
         self.fullName = fullName
         self.age = age
-        self.passwordNumber = passwordNumber
+        self.passportNumber = passportNumber
         self.passport = passport
         print("Инициализация человека \(fullName)")
     }
     
     deinit {
-        print("Деинициализация паспорта \(fullName)")
+        print("Деинициализация человека \(fullName)")
     }
 }
 
-
-// MARK: Тестирование
+// MARK: - Тестирование
 
 func testMemoryManagement() {
     // Замыкание чтобы объекты деинициализировались по завершении функции
     do {
         // Создание объекта человека
-        var person1: Person? = Person(fullName: "Иван Иванов", age: 30, passwordNumber: 123456)
+        var person1: Person? = Person(fullName: "Иван Иванов", age: 30, passportNumber: 123456)
         
         // Создание объекта паспорта
-        var passport1: Passport? = Passport(series: "AB", number: 123456, date: Date(), person: person1)
+        var passport1: Passport? = Passport(series: "AB", number: 123456, issueDate: Date(), person: person1)
         
         // Устанавливаем ссылку на паспорт в объекте Person
         person1?.passport = passport1
@@ -88,5 +98,6 @@ func testMemoryManagement() {
     // Деинициализация оставшихся объектов после выхода из замыкания
 }
 
-
+// Запуск тестирования
 testMemoryManagement()
+
